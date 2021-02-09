25783e36e30d422f041c340dd7d0bc59ca92b382
MaYingjian <742862395@qq.com>
2021-02-09 10:32:49 +0800

    增加遮蔽Pooling和输赢Pooling

--- src/module/Pooling.py
+++ src/module/Pooling.py
@@ -1,6 +1,27 @@
 from torch import nn
+from transformers import BertConfig
 
 
 class SequencePoolingV0(nn.Module):
     def forward(self, input):
         return input[0]
+
+
+class MLMPoolingV0(nn.Module):
+    def __init__(self, config: BertConfig):
+        super().__init__()
+        self.linear = nn.Linear(config.hidden_size, config.vocab_size)
+
+    def forward(self, input):
+        output = self.linear(input)
+        return output
+
+
+class WoLPoolingV0(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.linear = nn.Linear(config.hidden_size, 2)
+
+    def forward(self, input):
+        output = self.linear(input)
+        return output
